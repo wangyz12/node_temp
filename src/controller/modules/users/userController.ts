ae47de2a1ffd25f429711ebab5b0501ff80d0abc
youzhiwang <2277071555@qq.com>
2026-02-27 09:01:14 +0800

    fix:修改打印日志

--- src/controller/modules/users/userController.ts
+++ src/controller/modules/users/userController.ts
@@ -4,7 +4,7 @@ import { generateUserToken, generateUserTokenFromExisting } from '@/utils/userTo
 // 注册
 const register = async (req: ExpressRequest, res: ExpressResponse, next: ExpressNext) => {
   try {
-    logger.debug('创建用户请求', { body: req.body });
+    logger.success('创建用户请求', { body: req.body });
     // 直接从请求体创建用户
     const user = await UserModel.create(req.body);
     // 手动删除密码字段
@@ -41,12 +41,12 @@ const register = async (req: ExpressRequest, res: ExpressResponse, next: Express
 // 登录
 const login = async (req: ExpressRequest, res: ExpressResponse, next: ExpressNext) => {
   try {
-    logger.debug('用户登录', { body: req.body.account });
+    logger.success('用户登录', { body: req.body.account });
     const { account, password } = req.body;
     const user: any = await UserModel.findOne({ account }).select('+password');
     const isPasswordValid = user.comparePassword(password);
     if (!user || !isPasswordValid) {
-      logger.debug('用户名密码不正确', { account: req.body.account });
+      logger.error('用户名密码不正确', { account: req.body.account });
       return res.status(200).json({ code: 1000, data: null, msg: '用户名密码不正确' });
     }
     const tokenData = generateUserTokenFromExisting(user);
@@ -55,9 +55,9 @@ const login = async (req: ExpressRequest, res: ExpressResponse, next: ExpressNex
       msg: '用户创建成功',
       data: tokenData,
     });
-    logger.debug(`${req.body.account}用户登录成功`, { account: req.body.account });
+    logger.error(`${req.body.account}用户登录成功`, { account: req.body.account });
   } catch (error: any) {
-    logger.debug(`${req.body.account}用户登录失败`, error);
+    logger.error(`${req.body.account}用户登录失败`, error);
     return res.status(500).json({ code: 1000, data: null, msg: error });
   }
 };
