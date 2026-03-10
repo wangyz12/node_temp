7c279e573dec295e0c6ee92fac30060aaa416aa2
youzhiwang <2277071555@qq.com>
2026-03-10 17:48:47 +0800

    fix: 导出文件

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,4 +2,7 @@ export * from './test/test.model.ts';
 export * from './users/users.ts'; // user
 export * from './menu/menu.ts'; // 菜单
 export * from './dept/dept.ts'; // 部门
-export * from './captcha/captcha.ts';
+export * from './role/role.ts'; // 角色
+export * from './roleDept/roleDept.ts'; //角色关联部门
+export * from './roleMenu/roleMenu.ts'; // 角色关联菜单
+export * from './userRole/userRole.ts'; // 用户关联角色
